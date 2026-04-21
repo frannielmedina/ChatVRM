8a56413519d1f6f58144a0bbd7d51621822da7ef
Franniel <126989053+frannielmedina@users.noreply.github.com>
2026-04-21 13:37:08 -0500

    Add files via upload

--- src/features/koeiromap/koeiromap.ts
+++ src/features/koeiromap/koeiromap.ts
@@ -8,24 +8,11 @@ export async function koeiromapV0(
 ) {
   const param = {
     method: "POST",
-    body: JSON.stringify({
-      text: message,
-      speaker_x: speakerX,
-      speaker_y: speakerY,
-      style: style,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
+    body: JSON.stringify({ text: message, speaker_x: speakerX, speaker_y: speakerY, style }),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
   };
-
-  const koeiroRes = await fetch(
-    "https://api.rinna.co.jp/models/cttse/koeiro",
-    param
-  );
-
+  const koeiroRes = await fetch("https://api.rinna.co.jp/models/cttse/koeiro", param);
   const data = (await koeiroRes.json()) as any;
-
   return { audio: data.audio };
 }
 
@@ -36,29 +23,22 @@ export async function koeiromapFreeV1(
   style: "talk" | "happy" | "sad",
   apiKey: string
 ) {
-  // Request body
   const body = {
     text: message,
     speaker_x: speakerX,
     speaker_y: speakerY,
-    style: style,
+    style,
     output_format: "mp3",
   };
-
-  const koeiroRes = await fetch(
-    "https://api.rinna.co.jp/koeiromap/v1.0/infer",
-    {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
-        "Ocp-Apim-Subscription-Key": apiKey,
-      },
-    }
-  );
-
+  const koeiroRes = await fetch("https://api.rinna.co.jp/koeiromap/v1.0/infer", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache",
+      "Ocp-Apim-Subscription-Key": apiKey,
+    },
+  });
   const data = (await koeiroRes.json()) as any;
-
   return { audio: data.audio };
 }
