6039d0171cd9e66d3f84e2fa5b6943f9dfdf2ecc
Franniel <126989053+frannielmedina@users.noreply.github.com>
2026-04-21 13:42:29 -0500

    Add files via upload

--- src/lib/VRMAnimation/utils/linearstep.ts
+++ src/lib/VRMAnimation/utils/linearstep.ts
@@ -1,5 +1,2 @@
 import { saturate } from './saturate';
-
-export const linearstep = (a: number, b: number, t: number) => (
-  saturate((t - a) / (b - a))
-);
+export const linearstep = (a: number, b: number, t: number) => saturate((t - a) / (b - a));
